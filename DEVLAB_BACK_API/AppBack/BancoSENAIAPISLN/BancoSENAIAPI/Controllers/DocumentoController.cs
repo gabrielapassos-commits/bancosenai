21c8ab0875026837c6c7edb9e4eb96cc5fd47cb7
gabrielapassos-commits <gabriela.passos@aluno.senai.br>
2026-09-11 08:37:34 -0300

    feat(documentos): simulacao do banco de dados

--- DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
+++ DEVLAB_BACK_API/AppBack/BancoSENAIAPISLN/BancoSENAIAPI/Controllers/DocumentoController.cs
@@ -6,7 +6,8 @@ namespace BancoSENAIAPI.Controllers
     [Route("api/v1/[controller]")]
     public class DocumentoController : Controller
     {
-       
-       }
+        private readonly string _caminhoRaiz = Path.Combine(Directory.GetCurrentDirectory(), "clienteArquivo");
+        private static List<Models.DocumentoMetadados> _documentosMetadados = new List<Models.DocumentoMetadados>();
+        private static int _nextId = 1;
     }
 }
